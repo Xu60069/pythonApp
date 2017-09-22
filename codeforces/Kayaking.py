a9b69935783931ea6eac4cb142bcf327d9c90129
willwsxu <willwsxu@gmail.com>
2017-09-22 17:21:02 -0500

    kayaking, sort and complete search

--- codeforces/Kayaking.py
+++ codeforces/Kayaking.py
@@ -1,18 +1,39 @@
-def instability(nums):
-    nums=sorted(nums)
+# 2n numbers, 2n-2 pair, 2 singles
+# find the minimal sum of difference of all pairs
+# sort numbers, skip 2 numbers (n-1)*(n-1) ways
+# find minimal of sum of pair difference
+def strip2(nums, s1, s2):
+    res=[0]*(len(nums)-2)
+    j=0
+    for i in range(0, len(nums)):
+        if i==s1 or i==s2:
+            continue
+        res[j]=nums[i];
+        j += 1
+    return res
+
+def countPairwiseDiff(nums):
     total=0
-    maxdiff=0
-    for i in range(0, len(nums),2):
-        diff=nums[i+1]-nums[i]
-        total += diff
-        if diff>maxdiff:
-            maxdiff=diff
-    print(total-maxdiff)  # remove the largest
+    for i in range(1, len(nums), 2):
+        total += nums[i]-nums[i-1]
+    return total
+
+def completeSearch(nums):
+    nums=sorted(nums)
+    ans=1999999999
+    # skip two numbers, pair the rest of them in order
+    for skip1 in range(len(nums)-1):
+        for skip2 in range(skip1+1, len(nums)):
+            total=countPairwiseDiff(strip2(nums, skip1,skip2))
+            if total<ans:
+                ans=total
+    print(ans)  # remove the largest
 
 def test():
-    instability([101,100,99,5,4,3,2,1]); # 3
-    instability([1, 3, 4, 6, 3, 4, 100, 200]); # 5
-    instability([1, 2, 3, 40, 50, 99, 100, 101]); #12
+    completeSearch([101,100,99,5,4,3,2,1]); # 3
+    completeSearch([1, 3, 4, 6, 3, 4, 100, 200]); # 5
+    completeSearch([1, 2, 3, 40, 50, 99, 100, 101]); #12
+    completeSearch([1, 9, 10, 20, 100, 1000, 1001, 10001]); #19
 
 def nia():
     s=input()
@@ -26,6 +47,6 @@ def nia():
 
 def solve():
     n=input()
-    instability(nia())
+    completeSearch(nia())
 
-test()
+solve()
