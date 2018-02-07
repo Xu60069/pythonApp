1a9520724a157aad9ec711e05a90374f5a967e6d
willwsxu <willwsxu@gmail.com>
2018-02-07 16:11:22 -0600

    fix bug in cloingToys, add doc

--- codeforces/r461/CloningToys.py
+++ codeforces/r461/CloningToys.py
@@ -1,16 +1,23 @@
+#Each original toy can be cloned to create one additional original and one copy
+#Each copy can be cloned to create two additional copies
+#Is it possible to have toys of this many copies and original
 def cloing(copy, orig):
     if orig < 1:
         return False
     copyOrig=orig-1
     if copyOrig > copy:
         return False
+    if copyOrig==0 and copy>0: #special case, original not cloned
+        return False
     copy = copy-copyOrig
     return copy%2 ==0
 
 def test():
     print(cloing(6,3))
-    print(cloing(4,2))
+    print(cloing(4,2)==False)
     print(cloing(1000, 1001))
+    print(cloing(2,1)==False)  #failed case
+    print(cloing(0,1)) 
 
 def nia():
     s=input()
